1aa26b9c0053d68bfa141c92af720a9fe4d6b9f1
Burnt <mauricedix999@hotmail.com>
2020-05-13 14:08:25 +0930

    about page cleanup

--- WpfApp3/AboutWindow.xaml.cs
+++ WpfApp3/AboutWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Navigation;
 
 namespace WpfApp3
 {
@@ -23,5 +25,13 @@ namespace WpfApp3
         {
             InitializeComponent();
         }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+
     }
 }
