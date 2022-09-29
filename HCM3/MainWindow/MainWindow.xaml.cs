61c83cb03d895c3b2a7800453429fefc5bde060b
Burnt <mauricedix999@hotmail.com>
2022-09-30 03:58:22 +0930

    Having fun designing UI stuff

--- HCM3/MainWindow/MainWindow.xaml.cs
+++ HCM3/MainWindow/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Windows.Shapes;
 using System.Diagnostics;
 using BurntMemory;
 using HCM3.Commands;
+using HCM3.TestViews;
+using System.Collections.ObjectModel;
 
 namespace HCM3
 {
@@ -55,11 +57,20 @@ namespace HCM3
             HCMTasks tasks = new();
 
             //Setup command object that will be datacontext for binding of HCM's commands. They need the pointer collection and and a reference to MainWindow
-            HCM3.Commands.Commands CommandObject = new(pcollection, tasks);
-            Button1.DataContext = CommandObject;
+            //HCM3.Commands.Commands CommandObject = new(pcollection, tasks);
+            //Button1.DataContext = CommandObject;
 
-            
-            DataContext = this;
+
+            ObservableCollection<Checkpoint> custdata = new();
+            custdata.Add(new Checkpoint("ah", "ee"));
+            custdata.Add(new Checkpoint("ah", "essfe"));
+            custdata.Add(new Checkpoint("sadfasdfh", "ee"));
+            custdata.Add(new Checkpoint("ah", "esse"));
+            custdata.Add(new Checkpoint("asfsfh", "ee"));
+            DataContext = custdata;
+    
+
+           
 
 
 
@@ -78,6 +89,58 @@ namespace HCM3
 
     }
 
+    public class Checkpoint
+    {
+        public Checkpoint(string a, string b, string c = "Easy", string d = "00:01:28", string e = "22/09/03", string f = "2645")
+        { 
+            Name = a;
+            Level = b;
+            Diff = c;
+            IGT = d;
+            Created = e;
+            Version = f;
 
+        }
+        public int MinWidth = 10;
+        public string Name { get; set; }
+        public string Level { get; set; }
+        public string Diff { get; set; }
+        public string IGT { get; set; }
+        public string Created { get; set; }
+        public string Version { get; set; }
+
+    }
+
+
+
+    //yoink https://stackoverflow.com/a/61475351
+    public static class GridColumn
+    {
+        public static readonly DependencyProperty MinWidthProperty =
+            DependencyProperty.RegisterAttached("MinWidth", typeof(double), typeof(GridColumn), new PropertyMetadata(75d, (s, e) => {
+                if (s is GridViewColumn gridColumn)
+                {
+                    SetMinWidth(gridColumn);
+                    ((System.ComponentModel.INotifyPropertyChanged)gridColumn).PropertyChanged += (cs, ce) => {
+                        if (ce.PropertyName == nameof(GridViewColumn.ActualWidth))
+                        {
+                            SetMinWidth(gridColumn);
+                        }
+                    };
+                }
+            }));
+
+        private static void SetMinWidth(GridViewColumn column)
+        {
+            double minWidth = (double)column.GetValue(MinWidthProperty);
+
+            if (column.Width < minWidth)
+                column.Width = minWidth;
+        }
+
+        public static double GetMinWidth(DependencyObject obj) => (double)obj.GetValue(MinWidthProperty);
+
+        public static void SetMinWidth(DependencyObject obj, double value) => obj.SetValue(MinWidthProperty, value);
+    }
 
 }
