1d49eeacbff9208e56d537ae6f04e81f41f72376
Burnt <mauricedix999@hotmail.com>
2022-11-19 20:37:16 +1030

    serialisation & autodetection of H1 corebinfolder

--- HCM3/Services/Checkpoint/GetCoreBinPath.cs
+++ HCM3/Services/Checkpoint/GetCoreBinPath.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+using System.Diagnostics;
 
 namespace HCM3.Services
 {
@@ -11,10 +14,79 @@ namespace HCM3.Services
 
         public string GetCoreBinPath()
         {
+            Dictionary<string, string> coreBinFolderData = new();
+
+            string coreBinFolderDataLocation = Directory.GetCurrentDirectory() + "\\CoreBinFolder.xml";
+            if (File.Exists(coreBinFolderDataLocation))
+            {
+                try
+                {
+                    string xml = File.ReadAllText(coreBinFolderDataLocation);
+                    XDocument doc = XDocument.Parse(xml);
+
+                    foreach (XElement entry in doc.Root.Elements())
+                    {
+                        string? key = entry.Name.ToString();
+                        string? value = entry.Value.ToString();
+
+                        if (key != null && value != null)
+                        { 
+                            coreBinFolderData.Add(key, value);
+                        }
+                    }
+                }
+                catch
+                { }
+            }
+
+            string? currentVersion = this.HaloMemoryService.HaloState.CurrentAttachedMCCVersion;
+
+            if (currentVersion == null) throw new Exception("Failed to get corebinfolder path - wasn't attached to MCC");
+
+            if (coreBinFolderData.ContainsKey(currentVersion))
+            { 
+            return coreBinFolderData[currentVersion];
+            }
+
+            //else, let's try to autodetect the path
+            Process mcc = Process.GetProcessById((int)this.HaloMemoryService.HaloState.ProcessID);
+            string mccExePath = mcc.MainModule.FileName;
+
+            Trace.WriteLine("MCC EXE PATH: " + mccExePath);
+
+            if (!mccExePath.Contains(@"mcc\binaries\win64\MCC-Win64-Shipping.exe")) throw new Exception("Couldn't evaluate path of MCC process");
+
+            mccExePath = mccExePath.Replace(@"mcc\binaries\win64\MCC-Win64-Shipping.exe", @"core\core.bin");
+
+            if (!File.Exists(mccExePath)) throw new Exception("Couldn't autodetect core.bin folder path");
+
+            coreBinFolderData.Add(currentVersion, mccExePath);
+
+            SerialiseCoreBinData(coreBinFolderDataLocation, coreBinFolderData);
+
+            return mccExePath;
 
 
+        }
 
-            return @"C:\MCC2645 (s8)\Halo The Master Chief Collection\core\core.bin";
+        private void SerialiseCoreBinData(string coreBinFolderDataLocation, Dictionary<string, string> coreBinFolderData)
+        { 
+        if (File.Exists(coreBinFolderDataLocation)) File.Delete(coreBinFolderDataLocation);
+
+            using (TextWriter writer = File.CreateText(coreBinFolderDataLocation))
+            {
+                writer.WriteLine("<?xml version=\"1.0\" encoding=\"utf - 8\" ?>");
+                writer.WriteLine("<Root>");
+
+                foreach (KeyValuePair<string, string> kvp in coreBinFolderData)
+                {
+                    writer.WriteLine($"<{kvp.Key}>{kvp.Value}</{kvp.Key}>");
+
+                }
+                writer.WriteLine("</Root>");
+            }
+
+
         }
 
     }
