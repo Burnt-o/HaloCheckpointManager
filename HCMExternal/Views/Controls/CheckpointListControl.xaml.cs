37e103302cecbb402974dcd7e8b785103562e3a5
Burnt <mauricedix999@hotmail.com>
2023-10-14 20:12:13 +1030

    Checkpoint List now auto expands to fill available space - no more dead space to the right of "createdOn". This has been bugging me for literally years.

--- HCMExternal/Views/Controls/CheckpointListControl.xaml.cs
+++ HCMExternal/Views/Controls/CheckpointListControl.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HCMExternal.ViewModels;
+using Serilog;
 
 namespace HCMExternal.Views.Controls
 {
@@ -23,6 +27,55 @@ namespace HCMExternal.Views.Controls
         public CheckpointListControl()
         {
             InitializeComponent();
+        }
+
+        static double originalLastColumnWidth = 145;
+
+        // called when window containing the list view is resized. We want to expand/shrink the name column to fill the space.
+        private void ListView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            double currentColumnTotalWidth = 0;
+            foreach (GridViewColumn c in ((GridView)listView.View).Columns)
+            {
+                currentColumnTotalWidth += c.ActualWidth;
+            }
+
+            double newNameSize = ((GridView)listView.View).Columns[1].ActualWidth;
+            newNameSize += e.NewSize.Width - currentColumnTotalWidth - 10;
+
+            if (newNameSize > 55)
+                ((GridView)listView.View).Columns[1].Width = newNameSize;
+
+        }
+
+        private void ListView_Loaded(object sender, RoutedEventArgs e)
+        {
+            Log.Debug("LIST VIEW LOADED");
+           
+
+
+            // Add the handler to know when resizing a column is done
+            ((ListView)sender).AddHandler(Thumb.DragCompletedEvent, new DragCompletedEventHandler(ListViewHeader_DragCompleted), true);
+        }
+
+        private void ListViewHeader_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            double currentColumnTotalWidth = 0;
+            foreach (GridViewColumn c in ((GridView)listView.View).Columns)
+            {
+                currentColumnTotalWidth += c.ActualWidth;
+            }
+
+            if (currentColumnTotalWidth > listView.ActualWidth) return; // user wants scroll bar
+
+
+                double newNameSize = ((GridView)listView.View).Columns[1].ActualWidth;
+            newNameSize += listView.ActualWidth - currentColumnTotalWidth - 10;
+
+            if (newNameSize > 55)
+                ((GridView)listView.View).Columns[1].Width = newNameSize;
+
         }
     }
 }
