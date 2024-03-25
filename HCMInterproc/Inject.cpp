0932f40521686eaa9d2cb1e0611ba7760835ba4a
Burnt <mauricedix999@hotmail.com>
2024-03-26 00:52:48 +1030

    Improved dll injection logging: * VirtualAllocEx retries with PROCESS_ALL_ACCESS if it fails the first time. * Exceptions have added info on whether OpenProcess access rights were granted properly. * Exceptions have added info on what the security privilege of HCM/MCC is.

--- HCMInterproc/Inject.cpp
+++ HCMInterproc/Inject.cpp
@@ -14,6 +14,7 @@ public:
 		PLOG_ERROR << msg;
 	}
 	std::string what() { return message; }
+	void append(std::string ap) { message = message + ap; }
 };
 
 
@@ -154,65 +155,199 @@ bool processContainsModule(DWORD pid, std::wstring moduleName)
 	return false;
 }
 
+DWORD checkActualHandlePermissions(HANDLE handle)
+{
+	// certain driver-level anticheats can hook openProcess and strip certain access privledges.
+	// The only way to test for this (besides doing the actions that require those privledges and checking that they failed)
+	// is using NTQueryObject.
+	// https://learn.microsoft.com/en-us/windows/win32/api/winternl/nf-winternl-ntqueryobject
+
+	// NtQueryObject is not defined in any header files so we gotta grab it ourselves from ntdll.dll.
+	// ntdll.dll is technically loaded in every process but let's be safe and call loadLibrary anyway
+	auto ntHandle = LoadLibraryA("ntdll.dll");
+	if (!ntHandle) throw InjectionException(std::format("Could not get handle to ntdll ?!?! {}", GetErrorMessage(GetLastError())));
+
+	typedef NTSTATUS(WINAPI* NTQUERYOBJECT)(
+		HANDLE Handle,
+		OBJECT_INFORMATION_CLASS ObjectInformationClass,
+		PVOID ObjectInformation,
+		ULONG ObjectInformationLength,
+		PULONG ReturnLength);
+	
+	NTQUERYOBJECT pNtQueryObject = (NTQUERYOBJECT)GetProcAddress(ntHandle, "NtQueryObject");
+	if (!pNtQueryObject) throw InjectionException(std::format("Could not resolve NtQueryObject: {}", GetErrorMessage(GetLastError())));
+
+
+	PUBLIC_OBJECT_BASIC_INFORMATION obi;
+	if (0 <= pNtQueryObject(handle, ObjectBasicInformation, &obi, sizeof(obi), 0))
+	{
+		// ACCESS_MASK contains the specific rights in the lower half of the DWORD; these are the ones we care about so mask out the rest
+		return (obi.GrantedAccess & ~(0xFFFF0000));
+	}
+	else
+	{
+		throw InjectionException(std::format("NTQueryObject failed with: {}", GetErrorMessage(GetLastError())));
+	}
+
+	
+}
+
+// checks if process is elevated.
+std::string checkProcessElevation(DWORD pid)
+{
+	//Only need PROCESS_QUERY_LIMITED_INFORMATION.
+	HandlePtr secPrivHandleCheck(OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid));
+	if (!secPrivHandleCheck) return std::format("Could not open handle with PROCESS_QUERY_LIMITED_INFORMATION: {}", GetErrorMessage(GetLastError()));
+
+	HANDLE hToken = NULL;
+	if (!OpenProcessToken(secPrivHandleCheck.get(), TOKEN_QUERY, &hToken))
+		return std::format("OpenProcessToken failed: {}", GetErrorMessage(GetLastError()));
+
+	DWORD dwReturnLength = 0;
+	TOKEN_ELEVATION_TYPE tokenElevationType;
+	if (!GetTokenInformation(hToken, TokenElevationType, &tokenElevationType, sizeof(tokenElevationType), &dwReturnLength))
+		return std::format("GetTokenInformation failed: {}", GetErrorMessage(GetLastError()));
+
+	if (dwReturnLength != sizeof(tokenElevationType))
+		return std::format("Bad GetTokenInformation read, expected 0x{:X} bytes but got 0x{:X}", sizeof(tokenElevationType), dwReturnLength);
+
+	switch (tokenElevationType)
+	{
+	case TOKEN_ELEVATION_TYPE::TokenElevationTypeDefault:
+		return "Unlinked";
+
+	case TOKEN_ELEVATION_TYPE::TokenElevationTypeFull:
+		return "Admin";
+
+	case TOKEN_ELEVATION_TYPE::TokenElevationTypeLimited:
+		return "Normal";
+
+	default:
+		return std::format("TOKEN_ELEVATION_TYPE was invalid value: 0x{:X}", (DWORD)tokenElevationType);
+	}
+
+}
 
 void InjectModule(DWORD pid, std::string dllFilePath)
 {
-	HandlePtr mcc(OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, TRUE, pid));
-	if (!mcc) throw InjectionException(std::format("CEER didn't have appropiate permissions to modify MCC. If MCC or steam are running as admin, HCM needs to be run as admin too.\nNerdy details: InjectCEER: Couldn't open MCC with createRemoteThread permissions: {}", GetErrorMessage(GetLastError())).c_str());
-
-	// Get the address of our own Kernel32's loadLibrary (it will be the same in the target process because Kernel32 is loaded in the same virtual memory in all processes)
-	auto loadLibraryAddr = GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA");
-	if (!loadLibraryAddr) throw InjectionException(std::format("Couldn't find addr of loadLibraryA, error code: {}", GetErrorMessage(GetLastError())).c_str());
-
-
-
-	// Allocate some memory on the target process, enough to store the filepath of the DLL
-	auto pathAlloc = VirtualAllocEx(mcc.get(), 0, dllFilePath.size(), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	if (!pathAlloc) throw InjectionException(std::format("Failed to allocate memory in MCC for dll path, error code: {}", GetErrorMessage(GetLastError())).c_str());
-
-	// Write the dll filepath string to allocated memory
-	DWORD oldProtect;
-	DWORD dontcare;
-	size_t bytesWritten;
-	if (!VirtualProtectEx(mcc.get(), pathAlloc, dllFilePath.size(), PAGE_READWRITE, &oldProtect))
-		throw InjectionException(std::format("Failed to unprotect pathAlloc: {}", GetErrorMessage(GetLastError())).c_str());
-	if (!WriteProcessMemory(mcc.get(), pathAlloc, dllFilePath.c_str(), dllFilePath.size(), &bytesWritten))
-		throw InjectionException(std::format("Failed to write pathAlloc: {}", GetErrorMessage(GetLastError())).c_str());
-	if (!VirtualProtectEx(mcc.get(), pathAlloc, dllFilePath.size(), oldProtect, &dontcare))
-		throw InjectionException(std::format("Failed to reprotect pathAlloc: {}", GetErrorMessage(GetLastError())).c_str());
-	if (bytesWritten != dllFilePath.size())
-		throw InjectionException(std::format("Failed to completely write pathAlloc: {}", GetErrorMessage(GetLastError())).c_str());
-
-	PLOG_DEBUG << "Wrote path " << dllFilePath << "to MCC allocated memory at 0x" << std::hex << pathAlloc << "(0x" << bytesWritten << " bytes written)";
-
-	PLOG_DEBUG << "Calling createRemoteThread";
-	// Create a thread to call LoadLibraryA with pathAlloc as parameter
-	auto tHandle = CreateRemoteThread(mcc.get(), NULL, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(loadLibraryAddr), pathAlloc, NULL, NULL);
-	if (!tHandle) throw InjectionException(std::format("Couldn't create loadLibrary thread in mcc, error code: {}", GetErrorMessage(GetLastError())).c_str());
-
-	// Check if thread completed successfully
-	auto waitResult = WaitForSingleObject(tHandle, 3000);
-
-	switch (waitResult)
+	try
 	{
-	case 0x00000080:
-		throw InjectionException("Remote thread failed unexpectedly (WAIT_ABANDONED)");
-	case 0x00000102:
-		throw InjectionException("Remote thread timed out (WAIT_TIMEOUT)");
-	default:
-		break;
+
+		constexpr DWORD desiredMCCAccess = PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ;
+		HandlePtr mcc(OpenProcess(desiredMCCAccess, FALSE, pid));
+		if (!mcc) throw InjectionException(std::format("HCM didn't have appropiate permissions to modify MCC. If MCC or steam are running as admin, HCM needs to be run as admin too.\nNerdy details: {}", GetErrorMessage(GetLastError())).c_str());
+
+		try
+		{
+
+			// Get the address of our own Kernel32's loadLibrary (it will be the same in the target process because Kernel32 is loaded in the same virtual memory in all processes)
+			auto loadLibraryAddr = GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA");
+			if (!loadLibraryAddr) throw InjectionException(std::format("Couldn't find addr of loadLibraryA, error code: {}", GetErrorMessage(GetLastError())).c_str());
+
+
+			// Allocate some memory on the target process, enough to store the filepath of the DLL
+			auto pathAlloc = VirtualAllocEx(mcc.get(), 0, dllFilePath.size(), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+			if (!pathAlloc)
+			{
+				// attempt to open mcc with even more permissions
+				PLOG_INFO << "VirtualAllocEx failed with error: " << GetErrorMessage(GetLastError());
+				PLOG_INFO << "Re-attempting with high permissions";
+
+				mcc = HandlePtr(OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid));
+				if (!mcc) throw InjectionException(std::format("CEER didn't have appropiate permissions to modify MCC. If MCC or steam are running as admin, HCM needs to be run as admin too.\nNerdy details: InjectCEER: Couldn't open MCC with createRemoteThread permissions: {}", GetErrorMessage(GetLastError())).c_str());
+
+				pathAlloc = VirtualAllocEx(mcc.get(), 0, dllFilePath.size(), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+				if (!pathAlloc)
+				{
+					throw InjectionException(std::format("Failed to allocate memory in MCC for dll path, error code: {}", GetErrorMessage(GetLastError())).c_str());
+				}
+
+			}
+			// Write the dll filepath string to allocated memory
+			DWORD oldProtect;
+			DWORD dontcare;
+			size_t bytesWritten;
+			if (!VirtualProtectEx(mcc.get(), pathAlloc, dllFilePath.size(), PAGE_READWRITE, &oldProtect))
+				throw InjectionException(std::format("Failed to unprotect pathAlloc: {}", GetErrorMessage(GetLastError())).c_str());
+			if (!WriteProcessMemory(mcc.get(), pathAlloc, dllFilePath.c_str(), dllFilePath.size(), &bytesWritten))
+				throw InjectionException(std::format("Failed to write pathAlloc: {}", GetErrorMessage(GetLastError())).c_str());
+			if (!VirtualProtectEx(mcc.get(), pathAlloc, dllFilePath.size(), oldProtect, &dontcare))
+				throw InjectionException(std::format("Failed to reprotect pathAlloc: {}", GetErrorMessage(GetLastError())).c_str());
+			if (bytesWritten != dllFilePath.size())
+				throw InjectionException(std::format("Failed to completely write pathAlloc: {}", GetErrorMessage(GetLastError())).c_str());
+
+			PLOG_DEBUG << "Wrote path " << dllFilePath << "to MCC allocated memory at 0x" << std::hex << pathAlloc << "(0x" << bytesWritten << " bytes written)";
+
+			PLOG_DEBUG << "Calling createRemoteThread";
+			// Create a thread to call LoadLibraryA with pathAlloc as parameter
+			auto tHandle = CreateRemoteThread(mcc.get(), NULL, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(loadLibraryAddr), pathAlloc, NULL, NULL);
+			if (!tHandle) throw InjectionException(std::format("Couldn't create loadLibrary thread in mcc, error code: {}", GetErrorMessage(GetLastError())).c_str());
+
+			// Check if thread completed successfully
+			auto waitResult = WaitForSingleObject(tHandle, 3000);
+
+			switch (waitResult)
+			{
+			case 0x00000080:
+				throw InjectionException("Remote thread failed unexpectedly (WAIT_ABANDONED)");
+			case 0x00000102:
+				throw InjectionException("Remote thread timed out (WAIT_TIMEOUT)");
+			default:
+				break;
+			}
+
+			PLOG_DEBUG << "Remote thread finished execution";
+
+			// Get thread exit code 
+			DWORD exitCode;
+			GetExitCodeThread(tHandle, &exitCode);
+			if (exitCode == 0) throw InjectionException(std::format("LoadLibraryA failed: {}", GetErrorMessage(GetLastError())).c_str());
+
+			PLOG_DEBUG << "Remote thread exit code: 0x" << std::hex << exitCode;
+
+			PLOG_INFO << "Successfully injected module " << dllName;
+		}
+		catch (InjectionException ex) // Append more information to the exception using the process handle, then rethrow
+		{
+			// Check that the handle from openProcess was actually granted the permissions we requested.
+			// Certain driver-level anti-cheats will hook openProcess to remove certain permissions (without causing the function to fail)
+			try
+			{
+				DWORD grantedMCCAccess = checkActualHandlePermissions(mcc.get());
+
+				// need to do a saturating subtraction to see what rights are missing, if any. (prevent underflow if we were granted MORE permissions than explicitly desired)
+				// https://stackoverflow.com/questions/33481295/saturating-subtract-add-for-unsigned-bytes
+				DWORD missingRights = desiredMCCAccess - grantedMCCAccess;
+				missingRights &= -(missingRights <= desiredMCCAccess);
+
+				if (missingRights != 0)
+				{
+					ex.append(
+						std::format(
+							"\n\nGranted handle access was not desired handle access! \nThis probably (? not sure tbh) means an anti-cheat is running, stripping access rights. \nGranted access rights were: 0x{:04X}\nDifference is: 0x{:04X}", 
+							grantedMCCAccess, missingRights));
+				}
+			}
+			catch (InjectionException accessCheckEx)
+			{
+				ex.append(std::format("\n\nCould not double check process handles granted access bits: {}", accessCheckEx.what()));
+			}
+
+			throw ex;
+
+		}
 	}
+	catch (InjectionException ex) // Append security level info then rethrow
+	{
+		// no mcc handle here but we can still check security levels
+		ex.append("\n\nProcess Elevation Info: ");
+		ex.append(std::format("\nHCM elevation level: {}", checkProcessElevation(GetCurrentProcessId())));
+		ex.append(std::format("\nMCC elevation level: {}", checkProcessElevation(pid)));
 
-	PLOG_DEBUG << "Remote thread finished execution";
 
-	// Get thread exit code 
-	DWORD exitCode;
-	GetExitCodeThread(tHandle, &exitCode);
-	if (exitCode == 0) throw InjectionException(std::format("LoadLibraryA failed: {}", GetErrorMessage(GetLastError())).c_str());
-
-	PLOG_DEBUG << "Remote thread exit code: 0x" << std::hex << exitCode;
-
-	PLOG_INFO << "Successfully injected module " << dllName;
+		throw ex;
+	}
 }
 
 
