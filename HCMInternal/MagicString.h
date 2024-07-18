091d4fb3e40aff3d5933453e2873af6efe73aef9
Burnt <mauricedix999@hotmail.com>
2024-07-18 22:28:09 +0930

    Fix MagicString string constructor putting chars in backwards

--- HCMInternal/MagicString.h
+++ HCMInternal/MagicString.h
@@ -8,7 +8,7 @@ private:
 	int32_t num;
 public:
 	explicit MagicString(int32_t num) : num(num) {}
-	explicit MagicString(std::string str) { for (int i = 0; i < 4 && i < str.size(); i++) { chars[i] = str[i]; } }
+	explicit MagicString(std::string str) { for (int i = 0; i < 4 && i < str.size(); i++) { chars[i] = str.rbegin()[i]; } } // reverse!!
 	friend bool operator==(const auto& a, const auto& b) { return a.num == b.num; };
 
 	std::string_view getString() { return std::string_view(chars, 4); }
