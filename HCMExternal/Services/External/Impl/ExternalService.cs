423282a01a8ccc38b138f2c76848294b7a2bb155
Burnt <mauricedix999@hotmail.com>
2025-01-27 17:45:28 +1030

    Add host check for cartographer process

--- HCMExternal/Services/External/Impl/ExternalService.cs
+++ HCMExternal/Services/External/Impl/ExternalService.cs
@@ -9,6 +9,7 @@ using System.Diagnostics;
 using System.Printing.IndexedProperties;
 using System.Runtime.InteropServices;
 using Serilog;
+using System.Windows.Navigation;
 
 namespace HCMExternal.Services.External.Impl
 {
@@ -40,6 +41,31 @@ namespace HCMExternal.Services.External.Impl
             return null;
         }
 
+        bool is_host(HaloProcessInfo haloProcess)
+        {
+            if (haloProcess.processType != HaloProcessType.ProjectCartographer)
+                throw new Exception("Only project cartographer supported");
+
+
+
+
+            // get pointer to is_host enum
+            IMultilevelPointer p_is_host_enum = PointerData.GetGameProcessData<IMultilevelPointer>(haloProcess.processType, haloProcess.haloGame, "is_host_enum", haloProcess.processVersion);
+            byte is_host_enum = p_is_host_enum.readData(haloProcess.processHandle, 1)[0];
+
+            return (is_host_enum) switch
+            {
+                (0) => true, // Not in network session
+                (3) => false, // is NOT network session host
+                (5) => true, // is network session host
+                _ => throw new Exception("Invalid value for is_host_enum: " + (int)is_host_enum)
+            };
+
+
+
+
+        }
+
         // try to find cartographer, mccsteam, and mccwinstore processes. throws if it finds none.
         List<HaloProcessInfo> GetHaloProcessInfo()
         {
@@ -60,25 +86,32 @@ namespace HCMExternal.Services.External.Impl
                 };
 
 
-                haloProcessInfoOut.Add(new HaloProcessInfo()
+                var newHaloProcessInfo = new HaloProcessInfo()
                 {
                     processHandle = haloProcess,
                     processType = processType,
                     haloGame = processType == HaloProcessType.ProjectCartographer ? HaloGame.ProjectCartographer : throw new NotImplementedException("Only project cartographer supported"),
-                    processVersion = (processType) switch 
-                    { 
-                    (HaloProcessType.ProjectCartographer) => FindModule(haloProcess, "xlive.dll")?.FileVersionInfo.ToVerString() ?? PointerData.HighestSupportedCartographerVersion,
-                    (HaloProcessType.MCCSteam) => haloProcess.MainModule?.FileVersionInfo.ToVerString() ?? PointerData.HighestSupportedMCCVersion,
-                    (HaloProcessType.MCCWinStore) => PointerData.HighestSupportedMCCVersion, // can't downpatch winstore
+                    processVersion = (processType) switch
+                    {
+                        (HaloProcessType.ProjectCartographer) => FindModule(haloProcess, "xlive.dll")?.FileVersionInfo.ToVerString() ?? PointerData.HighestSupportedCartographerVersion,
+                        (HaloProcessType.MCCSteam) => haloProcess.MainModule?.FileVersionInfo.ToVerString() ?? PointerData.HighestSupportedMCCVersion,
+                        (HaloProcessType.MCCWinStore) => PointerData.HighestSupportedMCCVersion, // can't downpatch winstore
                         _ => throw new Exception("invalid HaloProcessType enum")
                     }
-                }
-                    );
+                };
+
+                if (is_host(newHaloProcessInfo))
+                    haloProcessInfoOut.Add(newHaloProcessInfo);
+                else
+                    Log.Debug("Skipping non-host process");
+               
             }
 
 
             if (haloProcessInfoOut.Count() == 0)
                 throw new Exception("Could not find any halo processes");
+            else
+                Log.Information(string.Format("Found {0} halo porcesses", haloProcessInfoOut.Count));
 
             return haloProcessInfoOut;
         }
